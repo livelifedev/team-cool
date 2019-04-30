959fb18d139deadbbe2febd5525090c733f58f8d
John <johnrubio93@gmail.com>
2019-04-30 11:24:41 +1000

    created before action for set_types for new and edit

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,6 @@
 class DocumentsController < ApplicationController
   before_action :set_document, only: [:show, :edit, :update, :destroy]
+  before_action :set_types, only: [:new, :edit]
 
   def index
     #get all available documents
