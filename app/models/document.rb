f1eea6a0cb888128f13de29ecab68d52c64ce5f1
John <johnrubio93@gmail.com>
2019-04-30 16:47:28 +1000

    fixed rating asscoiations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,7 @@
 class Document < ApplicationRecord
   has_many :document_subjects
   has_many :subjects, through: :document_subjects
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   enum doc_type: { essay: 0, presentation: 1 }
   validates :title, :description, :doc_type, presence: true
 end
