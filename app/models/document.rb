6fea7c359b9a6ada506625c74c68e37cd4c853ea
John <johnrubio93@gmail.com>
2019-04-30 14:25:15 +1000

    added validations for document creation

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,5 @@ class Document < ApplicationRecord
   has_many :document_subjects
   has_many :subjects, through: :document_subjects
   enum doc_type: { essay: 0, presentation: 1 }
+  validates :title, :description, :doc_type, presence: true
 end
