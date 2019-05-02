3a914bbbc932e961edba5dafdac0cc93cf81b023
John <johnrubio93@gmail.com>
2019-05-02 12:19:34 +1000

    linked the ratings to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_002525) do
+ActiveRecord::Schema.define(version: 2019_05_02_021354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,9 @@ ActiveRecord::Schema.define(version: 2019_05_02_002525) do
     t.bigint "document_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["document_id"], name: "index_ratings_on_document_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -84,4 +86,5 @@ ActiveRecord::Schema.define(version: 2019_05_02_002525) do
   end
 
   add_foreign_key "ratings", "documents"
+  add_foreign_key "ratings", "users"
 end
