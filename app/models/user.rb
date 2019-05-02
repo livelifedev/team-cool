1af2d970a56abe1080a2b1a6be5dbb19c0a7496c
John <johnrubio93@gmail.com>
2019-05-02 10:42:55 +1000

    added dependent destroy for documents

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :documents
+  has_many :documents, dependent: :destroy
 
   enum gender: { female: 0, male: 1, other: 2 }
   # validates :first_name, :surname, :gender, :date_of_birth, :university, presence: true
