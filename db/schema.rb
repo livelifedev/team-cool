b69769b667b7390265cf5d2790af4ac1767b091f
John <johnrubio93@gmail.com>
2019-05-02 10:03:42 +1000

    linked documents to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_062624) do
+ActiveRecord::Schema.define(version: 2019_05_01_235921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_062624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "doc_type"
+    t.integer "user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
