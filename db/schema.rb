442349b3d42f0064a45bab5cf43062acacc57f52
ggmchardie <ggmchardie@gmail.com>
2019-05-03 13:30:20 +1000

    styling to documents index made

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_05_02_110628) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,5 +115,4 @@ ActiveRecord::Schema.define(version: 2019_05_02_110628) do
   add_foreign_key "comments", "documents"
   add_foreign_key "comments", "users"
   add_foreign_key "documents", "users"
-
 end
