fb8bc07bf7fd9c4627eef7bb03f2d72f5512c54c
John <johnrubio93@gmail.com>
2019-05-03 20:36:46 +1000

    migration fix

--- db/migrate/20190429141604_create_document_subjects.rb
+++ db/migrate/20190429141604_create_document_subjects.rb
@@ -1,8 +1,8 @@
 class CreateDocumentSubjects < ActiveRecord::Migration[5.2]
   def change
     create_table :document_subjects do |t|
-      add_reference :document_subjects, :document, foreign_key: true
-      add_reference :document_subjects, :subject, foreign_key: true
+      t.integer :document_id
+      t.integer :subject_id
     end
   end
 end
