0ddd2f2c1a6e1479caa63c5badcb322e55505747
Nathan Tu <nathantuey@gmail.com>
2019-05-06 14:07:04 +1000

    Added Contact page for user enquiry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_112040) do
+ActiveRecord::Schema.define(version: 2019_05_06_024127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,11 +80,19 @@ ActiveRecord::Schema.define(version: 2019_05_05_112040) do
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
+  create_table "libraries", force: :cascade do |t|
+    t.integer "document_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reason"
   end
 
   create_table "ratings", force: :cascade do |t|
