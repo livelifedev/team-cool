f7e87a3f6e4633414a4dbc8f01c7dabaa1ffd3a6
Nathan Tu <nathantuey@gmail.com>
2019-05-06 10:28:34 +1000

    Auto stash before merge of "stripe2" and "origin/stripe2_edit"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+       
+       protect_from_forgery with: :exception
+
+       before_action :configure_permitted_parameters, if: :devise_controller?
+
+       rescue_from CanCan::AccessDenied do |exception|
+       redirect_to questions_path, :alert => exception.message
+end  
+     
+       protected
+
+        def configure_permitted_parameters
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :first_name, :surname, :username, :gender, :birthday, :school)}
+               devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password)}
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :surname, :username, :birthday, :gender, :email, :password, :current_password)}
+        end
+
+       
 end
