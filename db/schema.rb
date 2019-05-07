0b4ef042564b8a9224cfb00a1199e6b89d63d419
John <johnrubio93@gmail.com>
2019-05-07 22:09:39 +1000

    recreated database and remigrated

--- db/schema.rb
+++ db/schema.rb
@@ -80,15 +80,8 @@ ActiveRecord::Schema.define(version: 2019_05_06_024127) do
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
-  create_table "libraries", force: :cascade do |t|
-    t.integer "document_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "questions", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -125,7 +118,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_024127) do
     t.date "birthday"
     t.string "school"
     t.integer "gender"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "stripe_transaction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
